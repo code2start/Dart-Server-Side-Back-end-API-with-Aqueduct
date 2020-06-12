00043214f0597c7a84e1ee1ce125cd0d283caafb
Mohamed Taha <mohamed.taha@litpeaks.com>
2020-06-12 17:32:24 +0200

    Lesson 16

--- lib/controller/category_controller.dart
+++ lib/controller/category_controller.dart
@@ -28,4 +28,29 @@ class CategoryController extends ResourceController {
     final products = await q.fetch();
     return Response.ok(products);
   }
+
+  @Operation.post()
+  Future<Response> createCategory(
+      @Bind.body(ignore: ['id']) Category category) async {
+    final q = Query<Category>(context)..values = category;
+    final insertedCategory = await q.insert();
+    return Response.ok(insertedCategory);
+  }
+
+  @Operation.put('id')
+  Future<Response> updateCategory(
+      @Bind.path('id') int id, @Bind.body() Category category) async {
+    final q = Query<Category>(context)
+      ..where((c) => c.id).equalTo(id)
+      ..values = category;
+    final updatedCategory = await q.updateOne();
+    return Response.ok(updatedCategory);
+  }
+
+  @Operation.delete('id')
+  Future<Response> deleteCategory(@Bind.path('id') int id) async {
+    final q = Query<Category>(context)..where((c) => c.id).equalTo(id);
+    final affected = await q.delete();
+    return Response.ok({'message': 'There are $affected category removed'});
+  }
 }
